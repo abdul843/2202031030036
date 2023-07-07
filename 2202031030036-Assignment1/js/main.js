42e33fe8228949f3aeb4b52e9860eb237b25a051
abdul843 <71179019+abdul843@users.noreply.github.com>
2023-07-07 11:31:05 +0530

    Add files via upload

---
+++ 2202031030036-Assignment1/js/main.js
@@ -0,0 +1,10 @@
+// Typing Text
+var type = new Typed(".sec-text" ,{
+    strings: ["Front End Developer","Web Designer","Web Developer"],
+    typeSpeed: 90,
+    backSpeed:90,
+    loop: true
+})
+// Fade 
+
+// nav js 
